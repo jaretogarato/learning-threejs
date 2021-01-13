b00ac2f3cc8bdd1aad74fd1c4ebb756f0fed2e91
Jared Gallardo <jared@qualifiedigital.com>
2021-01-13 16:18:39 -0700

    move functionality from App to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ class Home extends Component {
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var material = new THREE.MeshBasicMaterial( { color: 0x22aaddee } );
     var cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
     camera.position.z = 5;
@@ -24,13 +24,14 @@ class Home extends Component {
     animate();
     // === THREE.JS EXAMPLE CODE END ===
   }
-  
+
   render() {
     return (
       <div />
     )
   }
 }
+
 
 const rootElement = document.getElementById("root");
 
